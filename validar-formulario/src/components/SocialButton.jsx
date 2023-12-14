5a228718477f2d2ae5142b64ac29c97fc9a80502
bladimirlpz <bladimir.lopez.p@gmail.com>
2023-12-14 15:35:09 -0300

    Act

--- validar-formulario/src/components/SocialButton.jsx
+++ validar-formulario/src/components/SocialButton.jsx
@@ -1,10 +1,10 @@
 const socialButtons = ({icono1,icono2,icono3}) => {
   return (
-    <a>
+    <>
       <button className="icon">{icono1}</button>
       <button className="icon">{icono2}</button>
       <button className="icon">{icono3}</button>
-    </a>
+    </>
   );
 };
 
