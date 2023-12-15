6de01209bcbd1c2678203d420a2cb9654b956fc3
bladimirlpz <bladimir.lopez.p@gmail.com>
2023-12-15 09:23:24 -0300

    -Se agrego luna constante para validar formato de nombre -Se agrego condicional para formato de nombre y sus errores. -Se ordenaron las posiciones de los condicionales para los errores

--- validar-formulario/src/components/Formulario.jsx
+++ validar-formulario/src/components/Formulario.jsx
@@ -8,28 +8,32 @@ const Formulario = ({ setError, setExito }) => {
   const [email, setEmail] = useState("");
   const [password_1, setPasswrod_1] = useState("");
   const [password_2, setPassword_2] = useState("");
-  const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
   const [errores, setErrores] = useState(false);
+  const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  const validNombre = /^([a-zA-Z]+)(\s[a-zA-Z]+)*$/;
 
   //Función que valida si las condiciones del formulario se cumplen para enviarlo con éxito
   const validarFormulario = (submit) => {
     //Función que evita que la página se recargue al presionar el botón de registro
     submit.preventDefault();
-
-    //Condicional que evalúa que ambas contraseñas sean iguales
-    if (password_1 !== password_2) {
-      return setErrores("Las contraseñas no son iguales");
+    //Condicional que evalúa  si el campo no esté vacío
+    if (nombre === ""){
+      return setErrores("Ingrese su nombre")
+      //Condicional que evalúa que el nombre tenga formato válido
+    } else if (!validNombre.test(nombre)) {
+      return setErrores("Formato de nombre invalido ingrese solo letras");
       //Condicional que evalúa que el mail sea válido y además que el campo no esté vacío
     } else if (!validEmail.test(email) && email.length > 0) {
       return setErrores("Formato de correo no válido");
       //Condicional que evalúa que la contraseña sea mínimo 10 caracteres y además el campo no esté vacío
     } else if (password_1.length > 1 && password_1.length < 10) {
       return setErrores("La contraseña debe tener mínimo 10 caracteres");
+      // Condicional que evalua los caracteres del nombre
+    } else if (password_1 !== password_2) {
+      return setErrores("Las contraseñas no coinciden");
     }
-
     //Reseteando errores al no cumplirse las condiciones de arriba
     setErrores("");
-
     //condicional que evalúa que los campos estén ok para enviar el formulario correctamente o mostrar error en caso de que no sea así
     if (
       nombre.length > 0 &&
@@ -44,7 +48,6 @@ const Formulario = ({ setError, setExito }) => {
       setExito("");
     }
   };
-
   //Apartado que renderiza el formulario exportado 
   return (
     <form className="form__container" onSubmit={(e) => validarFormulario(e)}>
